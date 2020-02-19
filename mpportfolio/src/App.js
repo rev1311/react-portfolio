cb8a90ae60b8859a4ac5ca16cc2e41a86a62eabc
Michael Partin <unmykx@gmail.com>
2020-02-19 12:55:32 -0800

    added links

--- mpportfolio/src/App.js
+++ mpportfolio/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import { Link } from 'react-router-dom';
 import Routes from './components/Routes';
 
 class App extends Component {
@@ -8,20 +9,20 @@ class App extends Component {
   return (
 <div style={{height: '300px', position: 'relative'}}>
     <Layout style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'}}>
-        <Header transparent title="Title" style={{color: 'white'}}>
+        <Header transparent title="Mike Partin" style={{color: 'white'}}>
             <Navigation>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
+                <Link to="/About">About</Link>
+                <Link to="/Resume">Resume</Link>
+                <Link to="/Projects">Projects</Link>
+                <Link to="/Contact">Projects</Link>
             </Navigation>
         </Header>
         <Drawer title="Title">
             <Navigation>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
+            <Link to="/About">About</Link>
+                <Link to="/Resume">Resume</Link>
+                <Link to="/Projects">Projects</Link>
+                <Link to="/Contact">Projects</Link>
             </Navigation>
         </Drawer>
         <Content>
