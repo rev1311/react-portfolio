6fbfdb5d4db9625a55c6a5c1ef46de2e414c19c7
Michael Partin <unmykx@gmail.com>
2020-02-19 12:20:00 -0800

    fixed imports

--- mpportfolio/src/App.js
+++ mpportfolio/src/App.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Nav from './Nav';
 import './App.css';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 
-function App() {
+class App extends Component {
+  render() {
   return (
-    <div className="App">
-      <Nav> 
-      <Router>
-        <Route exact path="./Gallery">Gallery</Route>
-        <Route exact path="./About">About</Route>
-        <Route exact path="./Contact">Contact</Route>
-      </Router>
-      </Nav>
-    </div>
-  );
-}
-
+<div style={{height: '300px', position: 'relative'}}>
+    <Layout style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'}}>
+        <Header transparent title="Title" style={{color: 'white'}}>
+            <Navigation>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+            </Navigation>
+        </Header>
+        <Drawer title="Title">
+            <Navigation>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+            </Navigation>
+        </Drawer>
+        <Content />
+    </Layout>
+</div>
+  )}};
 export default App;
