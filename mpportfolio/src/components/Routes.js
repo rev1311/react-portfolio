96f3bf5985918e2ca22ff7204e3ef811c91f27b5
Michael Partin <unmykx@gmail.com>
2020-02-19 12:42:01 -0800

    added routes

--- mpportfolio/src/components/Routes.js
+++ mpportfolio/src/components/Routes.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import Landing from './Landing';
 import About from './About';
+import Contact from './Contact';
+import Projects from './Projects';
+import Resume from './Resume';
+
 import { Switch, Route } from 'react-router-dom';
 
 
 const Routes = () => (
     <Switch>
-        <Route exact path="/" component={About} />
+        <Route exact path="/" component={Landing} />
+        <Route path="/About" component={About} />
+        <Route path="/Contact" component={Contact} />
+        <Route path="/Projects" component={Projects} />
+        <Route path="/Resume" component={Resume} />
     </Switch>
 )
 
