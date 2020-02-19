9322e95e9e1f95c43a616a1d5f56dbb9d3a90b3c
Michael Partin <unmykx@gmail.com>
2020-02-18 19:48:38 -0800

    started building imports and routes

--- mpportfolio/src/App.js
+++ mpportfolio/src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Nav from './Nav';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav> 
+      <Router>
+        <Route exact path="./Gallery">Gallery</Route>
+        <Route exact path="./About">About</Route>
+        <Route exact path="./Contact">Contact</Route>
+      </Router>
+      </Nav>
     </div>
   );
 }
