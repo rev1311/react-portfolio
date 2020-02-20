0ea278898c2e863cda2821d4dd059637bc6cb2e2
Michael Partin <unmykx@gmail.com>
2020-02-19 22:13:20 -0800

    added placeholder for resume page

--- mpportfolio/src/components/Resume.js
+++ mpportfolio/src/components/Resume.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+import '../App.css';
 
 class Resume extends Component {
     render() {
         return(
-            <div>resume page</div>
+            <div style={{ height: '90%', width: '80%', margin: 'auto', padding: '10px'}}>
+                <Grid>
+                    <Cell col={12}>
+                        <div className="resume">
+                            <h2>Resume</h2>
+                            <h4>coming soon</h4>
+                        </div>
+                    </Cell>
+                </Grid>    
+            </div>        
         )
     }
 }
