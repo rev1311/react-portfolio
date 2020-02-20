f5b1f9ca698527960a6167427f08e685186f631f
Michael Partin <unmykx@gmail.com>
2020-02-19 17:09:03 -0800

    added react-mdl grid

--- mpportfolio/src/components/Landing.js
+++ mpportfolio/src/components/Landing.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+
+const style = {
+    color: '#fff',
+    background: '#000',
+    padding: '10px',
+    margin: '10px',
+    borderradius: '10px'
+}
 
 class Landing extends Component {
     render() {
         return(
-            <div>Landing page</div>
+            <div style={{ width: '80%', margin: 'auto', background: '#ddd', padding: '10px'}}>
+                <Grid className="grid1" style={style}>
+                    <Cell col={12}>row 1</Cell>
+                </Grid>
+                <Grid className="grid2" style={style}>
+                    <Cell col={4}>row 2</Cell>
+                    <Cell col={4}>row 2</Cell>
+                    <Cell col={4}>row 2</Cell>
+                </Grid>
+            </div>
         )
     }
 }
