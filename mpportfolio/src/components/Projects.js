f8d45cd4e8293f051eff1076b281102064163235
Michael Partin <unmykx@gmail.com>
2020-02-19 22:13:04 -0800

    added placeholder for projects page

--- mpportfolio/src/components/Projects.js
+++ mpportfolio/src/components/Projects.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+import '../App.css';
 
 class Projects extends Component {
     render() {
         return(
-            <div>Projects page</div>
+            <div style={{ height: '90%', width: '80%', margin: 'auto', padding: '10px'}}>
+                <Grid>
+                    <Cell col={12}>
+                        <div className="projects">
+                            <h2>Projects</h2>
+                            <h4>coming soon</h4>
+                        </div>
+                    </Cell>
+                </Grid>    
+            </div>      
         )
     }
 }
