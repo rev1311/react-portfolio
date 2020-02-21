703cc0f2ce2f567173a50ae7b5f6fdf16123744f
Michael Partin <unmykx@gmail.com>
2020-02-20 16:48:15 -0800

    set the mapping to loop over projects to gen cards

--- mpportfolio/src/components/Projects.js
+++ mpportfolio/src/components/Projects.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
 import '../App.css';
+import ProjectCard from './ProjectCard';
+
 
 class Projects extends Component {
     render() {
         return(
-            <div style={{ height: '90%', width: '80%', margin: 'auto', padding: '10px'}}>
+            <div style={{ height: '100%', width: '80%', margin: 'auto', padding: '10px' }}>
                 <Grid>
                     <Cell col={12}>
                         <div className="projects">
                             <h2>Projects</h2>
-                            <h4>coming soon</h4>
                         </div>
                     </Cell>
-                </Grid>    
+                </Grid>
+                <Grid style={{ padding: '10px', margin: 'auto' }}>
+                    <ProjectCard />
+                </Grid> 
             </div>      
         )
     }
