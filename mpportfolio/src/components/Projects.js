6dbb736d0e1a3fa06b9976092fc8d8c2bd35b64b
Michael Partin <unmykx@gmail.com>
2020-02-20 16:53:25 -0800

    adjusted Grid

--- mpportfolio/src/components/Projects.js
+++ mpportfolio/src/components/Projects.js
@@ -8,13 +8,6 @@ class Projects extends Component {
     render() {
         return(
             <div style={{ height: '100%', width: '80%', margin: 'auto', padding: '10px' }}>
-                <Grid>
-                    <Cell col={12}>
-                        <div className="projects">
-                            <h2>Projects</h2>
-                        </div>
-                    </Cell>
-                </Grid>
                 <Grid style={{ padding: '10px', margin: 'auto' }}>
                     <ProjectCard />
                 </Grid> 
