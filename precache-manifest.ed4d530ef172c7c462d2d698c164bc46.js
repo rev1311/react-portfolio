c1fce89aa276aa239c1a5fb0d7cd253d9b1f6908
Michael Partin <unmykx@gmail.com>
2020-02-20 18:25:38 -0800

    Updates

---
+++ precache-manifest.ed4d530ef172c7c462d2d698c164bc46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c47469a2e3543bd49ef9117de8e186c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f680cd26590584a4f494",
+    "url": "/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "3a26ae4b488736606167",
+    "url": "/static/css/main.8b4a6a46.chunk.css"
+  },
+  {
+    "revision": "f680cd26590584a4f494",
+    "url": "/static/js/2.062f96ab.chunk.js"
+  },
+  {
+    "revision": "8956eb797ec1ca27a54b67665871977c",
+    "url": "/static/js/2.062f96ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a26ae4b488736606167",
+    "url": "/static/js/main.72d58f72.chunk.js"
+  },
+  {
+    "revision": "c92cf6721f9dfef372fe",
+    "url": "/static/js/runtime-main.ab6eb412.js"
+  }
+]);
